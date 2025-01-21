54367d9bc0e88a997f64ce294801e72a35f16625
Kallyne Layane <hellookallyne@gmail.com>
2025-01-21 20:41:51 -0300

    Implement Gameboard module
    This module is responsible by control gameboard functionalities.
    
    It implements a method to:
    - Get the gameboard.
    - Check if the gameboard is full.
    - Fill a gameboard's square.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // factory to create player objects
 const Player = (name, symbol) => ({ name, symbol });
+
+// module to create the game board
+const Gameboard = (() => {
+  const gameboard = Array(9).fill('');
+
+  const getGameboard = () => gameboard;
+
+  const isGameboardFull = () => gameboard.every(square => square !== '');
+
+  const fillSquare = (position, symbol) => {
+    if (gameboard[position] !== '') {
+      throw new Error(`The position ${position} is already marked. Try again.`);
+    }
+    gameboard[position] = symbol;
+  };
+
+  return { getGameboard, isGameboardFull, fillSquare };
+})();
