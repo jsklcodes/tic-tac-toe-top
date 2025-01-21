99a84f56cd6c55772d2828c2f6c231f86fe928a6
Kallyne Layane <hellookallyne@gmail.com>
2025-01-21 20:47:59 -0300

    Implement Game module
    This module is responsible by control the game flow.
    
    It defines the two players and the current player. And implements the
    methods to:
    - Get the current player.
    - Switch the current player.
    - Check in case of victory.
    - Check in case of draw.
    - Make a move.

--- index.js
+++ index.js
@@ -18,3 +18,65 @@ const Gameboard = (() => {
 
   return { getGameboard, isGameboardFull, fillSquare };
 })();
+
+// module to control the game flow
+const Game = (() => {
+  const players = [Player('Linus', 'X'), Player('Emily', 'O')];
+  let currentPlayer = players[0];
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  const switchCurrentPlayer = () => {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+  };
+
+  const hasVictory = () => {
+    const gameboard = Gameboard.getGameboard();
+    const victoryCombo = [
+      // rows
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // diagonals
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < victoryCombo.length; i++) {
+      const [a, b, c] = victoryCombo[i];
+      if (
+        gameboard[a] &&
+        gameboard[a] === gameboard[b] &&
+        gameboard[a] === gameboard[c]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const hasDraw = () => {
+    const { isGameboardFull } = Gameboard;
+    return !hasVictory() && isGameboardFull();
+  };
+
+  const makeMove = position => {
+    try {
+      Gameboard.fillSquare(position, currentPlayer.symbol);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return {
+    getCurrentPlayer,
+    switchCurrentPlayer,
+    makeMove,
+    hasVictory,
+    hasDraw,
+  };
+})();
